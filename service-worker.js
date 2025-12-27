0eb23c397b8447f8411e101149609db850cf776d
deliciasfedde <deliciasfedde@gmail.com>
2025-12-27 05:12:00 -0300

    Crear service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "finanzas-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./offline.html",
+  "./css/styles.css",
+  "./js/app.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      })
+      .catch(() => caches.match(event.request).then(r => r || caches.match("./offline.html")))
+  );
+});
